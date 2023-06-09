c0a24d063703dbfbbb0954a86c988863c99dc137
machikomomo <3084096374@qq.com>
2023-06-09 22:22:02 +0800

    demo

--- src/main/java/cn/odyssey/reggie/demo/TestDemo.java
+++ src/main/java/cn/odyssey/reggie/demo/TestDemo.java
@@ -1,4 +1,8 @@
 package cn.odyssey.reggie.demo;
 
 public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("demo here");
+        System.out.println("demo next");
+    }
 }
---
+++ src/main/java/cn/odyssey/reggie/demo/TestDemo2.java
@@ -0,0 +1,7 @@
+package cn.odyssey.reggie.demo;
+
+public class TestDemo2 {
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
