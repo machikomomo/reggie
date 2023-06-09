e4ed072cdd5a335f6f7d2e0ab89391e706f7d1e2
machikomomo <3084096374@qq.com>
2023-06-09 22:52:08 +0800

    b1

--- src/main/java/cn/odyssey/reggie/demo/TestDemoB1.java
+++ src/main/java/cn/odyssey/reggie/demo/TestDemoB1.java
@@ -3,5 +3,6 @@ package cn.odyssey.reggie.demo;
 public class TestDemoB1 {
     public static void main(String[] args) {
         System.out.println("b1");
+        System.out.println("merge");
     }
 }
